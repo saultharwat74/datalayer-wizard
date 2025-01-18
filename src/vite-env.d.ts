a8605848b2b3c6c4a21d3e9f0fb04530170a0c84
Muhammad Tharwat <mohamedtharout190@gmail.com>
2025-01-18 23:04:35 +0200

    feat: enhance type definitions

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,23 @@
 /// <reference types="vite/client" />
 
-declare module "*.vue" {
-  import type { DefineComponent } from "vue";
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+}
 
-  const component: DefineComponent<{}, {}, any>;
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $filters: Record<string, (value: unknown) => string>;
+  }
+}
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue';
+
+  const component: DefineComponent<Record<string, unknown>, Record<string, unknown>, unknown>;
 
   export default component;
 }
