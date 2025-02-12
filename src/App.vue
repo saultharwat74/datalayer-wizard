9763430500c397a734746ddb3c6f92c0eb9b9294
Muhammad Tharwat <mohamedtharout190@gmail.com>
2025-02-12 22:48:42 +0200

    feat: implement Toast components and integrate Toaster into App

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
   import MainLayout from '@/layouts/MainLayout.vue';
+  import { Toaster } from './components/ui/toast';
 </script>
 
 <template>
+  <Toaster />
   <MainLayout />
 </template>
 
